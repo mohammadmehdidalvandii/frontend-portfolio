4acf7c8f72a41621801a104ec7f16f8396c401cb
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-08 15:06:32 +0330

    created Dialog model

---
+++ src/components/UI/Dialog.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import {X} from 'lucide-react';
+import { cn } from '@utils/cn';
+
+
+const Dialog = DialogPrimitive.Root;
+const DialogTrigger = DialogPrimitive.Trigger;
+const DialogPortal = DialogPrimitive.Portal;
+const DialogClose = DialogPrimitive.Close;
+
+const DialogOverlay = React.forwardRef<
+React.ElementRef<typeof DialogPrimitive.Overlay>,
+React.ComponentPropsWithRef<typeof DialogPrimitive.Overlay>
+>(({className , ...props} ,ref)=>(
+    <DialogPrimitive.Overlay
+        ref={ref}
+        className={cn(
+            'fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+            className,
+            {...props}
+        )}
+    />
+));
+
+DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
+
+const DialogContent = React.forwardRef<
+React.ElementRef<typeof DialogPrimitive.Content>,
+React.ComponentPropsWithRef<typeof DialogPrimitive.Content>
+>(({className , children , ...props},ref)=>(
+    <DialogPortal>
+        <DialogOverlay/>
+        <DialogPrimitive.Content
+            ref={ref}
+            className={cn(
+                'fixed left-[50%] top-[50%] z-50 grid w-full maw-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-bg p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg',
+                className 
+            )}
+            {...props}
+        >
+            {children}
+            <DialogPrimitive.Close
+                className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-bg cursor-pointer transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-primary data-state:open:text-accent'
+            >
+                <X className='h-4 w-4'/>
+                <span className='sr-only'>Close</span>
+            </DialogPrimitive.Close>
+        </DialogPrimitive.Content>
+    </DialogPortal>
+));
+
+DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+const DialogHeader = ({className , ...props}:React.HTMLAttributes<HTMLDivElement>)=>(
+    <div
+        className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2' , className)}
+        {...props}
+    />
+);
+
+DialogHeader.displayName = 'DialogHeader';
+
+const DialogFooter = ({className , ...props}:React.HTMLAttributes<HTMLDivElement>)=>(
+    <div
+        className={cn('flex flex-col-reverse  sm:flex-row sm:justify-end sm:space-x-2', className)}
+        {...props}
+    />
+)
+DialogFooter.displayName = 'DisplayFooter';
+
+const DialogTitle = React.forwardRef<
+    React.ElementRef<typeof DialogPrimitive.Title>,
+    React.ComponentPropsWithRef<typeof DialogPrimitive.Title>
+>(({className , ...props},ref)=>(
+    <DialogPrimitive.Title
+        ref={ref}
+        className={cn(
+            'text-lg font-semibold font-mono leading-none tracking-tight',className
+        )}
+        {...props}
+    />
+));
+
+DialogTitle.displayName = DialogPrimitive.Title.displayName;
+
+const DialogDescription = React.forwardRef<
+    React.ElementRef<typeof DialogPrimitive.Description>,
+    React.ComponentPropsWithRef<typeof DialogPrimitive.Description>
+>(({className , ...props} , ref)=>(
+    <DialogPrimitive.Description
+        ref={ref}
+        className={cn('text-sm text-accent',className)}
+        {...props}
+    />
+));
+
+DialogDescription.displayName = DialogPrimitive.Description.displayName;
+
+export{
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogTrigger,
+    DialogContent,
+    DialogClose,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+}
