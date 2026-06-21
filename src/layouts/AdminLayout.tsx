1d5c5874dcfec11d818683d50f5b197bb369d270
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-21 15:51:06 +0330

    fixed access to admin router

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,8 +1,15 @@
 import Footer from "@components/modules/Footer/Footer"
 import Navigation from "@components/modules/Navigation/Navigation"
-import { Outlet } from "react-router-dom"
+import { showError } from "@utils/Toasts";
+import { Outlet , Navigate} from "react-router-dom"
 
 export default function AdminLayout() {
+    const token = document.cookie.includes('token');
+    if(!token) {
+      showError('Access denied. Please login first')
+      return <Navigate to='/' replace/>
+    }
+
   return (
     <div className="min-h-screen">
         <Navigation/>
