cc5bb23bbaf8d9b5d56cffc848f85dce57c2fbbf
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-22 15:40:41 +0330

    fixed project-page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,7 +1,13 @@
 import ProjectCard from "@components/modules/ProjectCard/ProjectCard"
+import { useGetAllProject } from "@services/project.services";
 import { Helmet } from "react-helmet-async"
+import { ProjectDTO } from "../../types/project";
 
 function Projects() {
+    const {data:Projects , isError , isLoading} = useGetAllProject();
+  
+      if (isLoading) return <p className="font-mono text-center text-primary">// loading...</p>;
+    if (isError) return <p className="font-mono text-center text-primary">// error fetching projects</p>;
   return (
     <>
       <Helmet>
@@ -18,10 +24,27 @@ function Projects() {
             </h1>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-px bg-input border-y border-input">
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/> 
+      {Projects.length === 0 ? (
+        <p className="font-mono text-center text-primary">// no projects found</p>
+      ) : (
+        Projects.map((project:ProjectDTO)=>(
+          <ProjectCard 
+            key={project._id}
+            _id={project._id}
+            name={project.name}
+            title={project.title}
+            stacks={project.stacks}
+            image={project.image}
+            demoLink={project.demoLink}
+            githubLink={project.githubLink}
+            year={project.year}
+            role={project.role}
+            features={project.features}
+            shortDescription={project.shortDescription}
+            longDescription={project.longDescription}
+          />
+        ))
+      )}
           </div>
     </div>
     </>
