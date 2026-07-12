33fb5b5d19ca52ac8b8be9e95ca735fa3fa39c59
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-07-12 08:16:57 +0330

    change api routes

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,14 +2,14 @@ import { getToken } from '@utils/auth';
 import axios from 'axios';
 
 export const publicApi = axios.create({
-    baseURL:"https://protfolio-back.chbkn.run/api",
+    baseURL:"https://protfolio-back.chbkn.dev/api",
 });
 
 export const privateApi = axios.create({
-    baseURL:"https://protfolio-back.chbkn.run/api",
+    baseURL:"https://protfolio-back.chbkn.dev/api",
 })
 
-privateApi.interceptors.request.use((config)=>{
+privateApi.interceptors.request.use((config:any)=>{
     const token = getToken();
     if(token){
         config.headers.Authorization = `Bearer ${token}`
